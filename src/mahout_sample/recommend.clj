eb42afb1e5de1e2c3fc63e6d0d2330e54e885f88
Paul Ingles <paul@oobaloo.co.uk>
2011-03-30 21:51:03 +0100

    Add estimate-user-preference fn

--- src/mahout_sample/recommend.clj
+++ src/mahout_sample/recommend.clj
@@ -5,6 +5,7 @@
            [org.apache.mahout.cf.taste.impl.model.file FileDataModel]
            [org.apache.mahout.cf.taste.impl.similarity LogLikelihoodSimilarity]
            [org.apache.mahout.cf.taste.impl.neighborhood ThresholdUserNeighborhood]
+           [org.apache.mahout.cf.taste.recommender UserBasedRecommender]
            [org.apache.mahout.cf.taste.impl.recommender GenericUserBasedRecommender GenericItemBasedRecommender]))
 
 (defprotocol ToClojure
@@ -16,13 +17,20 @@
                                                     :value (.getValue x)}))
 
 (defn user-recommender
+  "Creates a file based user-recommender"
   [file]
   (let [model (FileDataModel. file)
         similarity (LogLikelihoodSimilarity. model)]
     (GenericUserBasedRecommender. model (ThresholdUserNeighborhood. 0.5 similarity model) similarity)))
 
 (defn user-recommendations
-  ([r u]
+  "Using recommender r, generates a sequence of n recommended item id's and their value."
+  ([^UserBasedRecommender r u]
      (recommendations r u 10))
-  ([r u n]
+  ([^UserBasedRecommender r u n]
      (to-clojure (.recommend r u n))))
+
+(defn estimate-user-preference
+  "Using recommender r, estimates user u's preference for item i."
+  [^UserBasedRecommender r u i]
+  (.estimatePreference r u i))
